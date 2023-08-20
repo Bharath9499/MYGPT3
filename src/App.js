68e517804edec58d1096dea04caf9cc649c0297d
Bharath9499 <bharathm941999@gmail.com>
2023-08-20 11:44:11 +0530

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {Footer , Blog , Possibility , Features , Whatgpt3 , Header} from './containers'
+import { Brand , CTA , Navbar}from'./components'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="gradient__bg">
+        <Navbar />
+        <Header />
+        </div>
+         <Brand />
+         <Whatgpt3 />
+         <Features />
+         <Possibility />
+         <CTA />
+         <Blog />
+         <Footer />
     </div>
   );
 }
